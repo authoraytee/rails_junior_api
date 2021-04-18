905eed882aaf91cb8a0ca7bb7990d1348136d8cb
authoraytee <authoraytee@mail.ru>
2021-04-18 18:49:06 +0700

    First attempts at model validation

--- app/models/diary.rb
+++ app/models/diary.rb
@@ -1,12 +1,10 @@
 # frozen_string_literal: true
 
 class Diary < ApplicationRecord
-  has_many :notes, dependent: :delete_all
-  validates :title, presence: true
+  has_many :notes, inverse_of: :diary, dependent: :delete_all
+
+  validates :title, presence: true, length: { in: 5..300 }
   validates :expiration, presence: true
 
-  enum kind: {
-    for_all: 2,
-    personal: 1
-  }
+  enum kind: { for_all: 2, personal: 1}, presence: true
 end
--- app/models/note.rb
+++ app/models/note.rb
@@ -1,4 +1,9 @@
 # frozen_string_literal: true
 
 class Note < ApplicationRecord
+    belongs_to :diary, index: true, foreign_key: true
+    validates_associated :diary
+
+    validates :text, presence: true, length: { in: 5..300 }, allow_nil: true
+    validates :diary_id, presence: true, allow_nil: true
 end
