8a65925168a61927dad24b78b939af6bf284ad28
authoraytee <authoraytee@gmail.com>
2021-04-21 09:12:22 +0700

    new model validation

--- app/models/diary.rb
+++ app/models/diary.rb
@@ -1,10 +1,12 @@
 # frozen_string_literal: true
 
 class Diary < ApplicationRecord
-  has_many :notes, inverse_of: :diary, dependent: :delete_all
-
-  validates :title, presence: true, length: { in: 5..300 }
+  has_many :notes, dependent: :delete_all
+  validates :title, presence: true
   validates :expiration, presence: true
 
-  enum kind: { for_all: 2, personal: 1}, presence: true
+  enum kind: {
+    for_all: 2,
+    personal: 1
+  }
 end
